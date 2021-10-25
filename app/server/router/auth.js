cb601d4c6881defa7958d7815c0189931d1ea2c2
AdrianPC <adrianchooo18@gmail.com>
2021-10-25 12:44:25 -0500

    endpoints register, login and token valiated first try created

--- app/server/router/auth.js
+++ app/server/router/auth.js
@@ -4,11 +4,27 @@ const {Router} = require('express');
 
 const router = Router();
 
-//Creating endpoints
+//Creating endpoints new users
 router.post('/new', (req, res) => {
     res.json({
         ok: true,
-        usuario: 'ABC'
+        msg: 'new'
+    });
+});
+
+//Creating Login
+router.post('/', (req, res) => {
+    res.json({
+        ok:true,
+        msg: 'login'
+    });
+});
+
+//Revalidate token
+router.get('/renew', (req, res) => {
+    res.json({
+        ok: true,
+        msg: 'renew'
     });
 });
 
