34a0b74195d4417a9a203020bebfa10c18cbeae3
AdrianPC <adrianchooo18@gmail.com>
2021-10-25 21:07:07 -0500

    user action in db is on

--- app/server/controllers/auth.js
+++ app/server/controllers/auth.js
@@ -1,18 +1,49 @@
 //Making controllers for Login
 const { response} = require('express');
+//Imports models user
+const User = require('../models/user');
+
 
 
 //Add user
 const addUser = async(req, res = response) => {
 
-    const {nombre, password, email} = req.body;
-    res.json({
-        ok: true,
-        msg: 'new',
-        nombre,
-        password,
-        email
-    });
+    try {
+        const {email, password} = req.body;
+        //Check email doesn't exist
+        const presentEmail = await User.findOne({email});
+        if (presentEmail) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo ya existe'
+            });
+        }
+
+        // Encrypt password
+
+        //Save user in BD
+        const user = new User(req.body);
+        await user.save();
+
+        res.json({
+            user
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Contacte al administrador'
+        });
+    }
+    // const {nombre, password, email} = req.body;
+    // res.json({
+    //     ok: true,
+    //     msg: 'new',
+    //     nombre,
+    //     password,
+    //     email
+    // });
 };
 
 //Login User
