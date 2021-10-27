cf2fc80902ccf6bd9d2018acab667133e769fb8d
James Efrain Rodriguez Fonseca <jamesrodriguez3188@gmail.com>
2021-10-26 21:22:50 -0500

    creacion components ChatSelect

--- app/cliente/src/components/IncomingMessage.js
+++ app/cliente/src/components/IncomingMessage.js
@@ -11,7 +11,7 @@ export const IncomingMessage = () => {
             </div>
             <div className="received_msg">
                 <div className="received_withd_msg">
-                    <p>Test which is a new approach to have all solutions</p>
+                    <p>Hola, prueba nuestro chat para tener soluciones</p>
                     <span className="time_date">11:01 AM | June 9</span>
                 </div>
             </div>
--- app/cliente/src/components/OutgoingMessage.js
+++ app/cliente/src/components/OutgoingMessage.js
@@ -4,7 +4,7 @@ export const OutgoingMessage = () => {
     return (
         <div className="outgoing_msg">
             <div className="sent_msg">
-                <p>Test which is a new approach to have all solutions</p>
+                <p>hola, perfecto, gracias</p>
                 <span className="time_date">11:01 AM | June 9</span>
             </div>
         </div>
