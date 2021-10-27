dccfcbf7ceaae16c08f5666dcf2a5f2ba51015ad
James Efrain Rodriguez Fonseca <jamesrodriguez3188@gmail.com>
2021-10-26 20:57:39 -0500

    creacion componente outgoinmessage

--- app/cliente/src/components/Messages.js
+++ app/cliente/src/components/Messages.js
@@ -1,41 +1,21 @@
 import React from "react";
+import { IncomingMessage } from "./IncomingMessage";
+import { OutgoingMessage } from "./OutgoingMessage";
 import { SendMessage } from "./SendMessage";
 
 export const Messages = () => {
+    const msgs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     return (
         <div className="mesgs">
             {/*Historia inicio */}
             <div className="msg_history">
-                {/*Mensaje recibido Inicio */}
-                <div className="incoming_msg">
-                    <div className="incoming_msg_img">
-                        <img
-                            src="https://ptetutorials.com/images/user-profile.png"
-                            alt="sunil"
-                        />
-                    </div>
-                    <div className="received_msg">
-                        <div className="received_withd_msg">
-                            <p>
-                                Test which is a new approach to have all
-                                solutions
-                            </p>
-                            <span className="time_date">11:01 AM | June 9</span>
-                        </div>
-                    </div>
-                </div>
-                {/*Mensaje recibido Fin */}
-
-                {/*Mensaje enviado inicio */}
-                <div className="outgoing_msg">
-                    <div className="sent_msg">
-                        <p>
-                            Test which is a new approach to have all solutions
-                        </p>
-                        <span className="time_date">11:01 AM | June 9</span>
-                    </div>
-                </div>
-                {/*Mensaje enviado inicio */}
+                {msgs.map((msgs) =>
+                    msgs % 2 ? (
+                        <IncomingMessage key={msgs} />
+                    ) : (
+                        <OutgoingMessage key={msgs} />
+                    )
+                )}
             </div>
             {/*Historia Fin */}
 
